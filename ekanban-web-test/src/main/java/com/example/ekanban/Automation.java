a8233d6d65bc4a25dcd7a8201e4b657b6283dfb6
Md Zahid Raza <zahid7292@ymail.com>
2017-05-21 16:28:49 +0530

    added utility class for automation module

--- ekanban-web-test/src/main/java/com/example/ekanban/Automation.java
+++ ekanban-web-test/src/main/java/com/example/ekanban/Automation.java
@@ -1,7 +1,14 @@
 package com.example.ekanban;
 
+import com.example.ekanban.util.AutomationUtil;
+import com.example.ekanban.util.Constants;
+
 /**
  * Created by mtalam on 5/21/2017.
  */
 public class Automation {
+
+    public static void main(String[] args){
+        System.out.println(AutomationUtil.getProperty(Constants.USER_NAME));
+    }
 }
---
+++ ekanban-web-test/src/main/java/com/example/ekanban/util/Constants.java
@@ -0,0 +1,9 @@
+package com.example.ekanban.util;
+
+/**
+ * Created by mdzahidraza on 21/05/17.
+ */
+public class Constants {
+
+    public static final String USER_NAME = "user.name";
+}
